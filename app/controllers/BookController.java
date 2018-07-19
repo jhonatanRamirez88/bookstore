9be1578a527954e00c6151627f58fc765e284662
jonas <tonny_jho@hotmail.com>
2018-07-18 23:36:59 -0500

    Creacion de controller de libros

---
+++ app/controllers/BookController.java
@@ -0,0 +1,35 @@
+package controllers;
+import play.mvc.*;
+
+
+
+public class BookController extends Controller{
+
+    public Result index(){
+        return TODO;
+    }
+    
+    public Result create(){
+        return TODO;
+    }
+    
+    public Result save(){
+        return TODO;
+    }
+    
+    public Result edit(Integer id){
+        return TODO;
+    }
+    
+    public Result update(){
+        return TODO;
+    }
+    
+    public Result destroy(Integer id){
+        return TODO;
+    }
+    
+    public Result show(Integer id){
+        return TODO;
+    }
+}
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-java-starter-example"""
+name := """bookstore"""
 
 version := "1.0-SNAPSHOT"
 
